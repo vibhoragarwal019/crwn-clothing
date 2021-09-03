a4e349ff8ec831f7591095e2a1d2367174b57c50
vibhor <agarwalvibhor019@gmail.com>
2021-09-03 11:48:47 +0530

    added redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 
@@ -31,4 +32,8 @@ const Header = ({ currentUser }) => (
     </div>
   );
 
-export default Header;
+  const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+  });
+  
+  export default connect(mapStateToProps)(Header);
