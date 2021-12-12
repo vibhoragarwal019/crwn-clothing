57ebaaa7f8abf11c6b83b5d5e6f24da5a6f8ef60
vibhor <agarwalvibhor019@gmail.com>
2021-12-12 18:15:52 +0530

    integrate stripe and favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HU5vfCraKRWjr4tJ9qjm3ranxskshWOOPu9TArvgPKkAh89pWX7kAVTaSAXXlIjkXWqVpMFZAwdkPpY8HFvTnwd00D5Ll8PVl';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Vibhor Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
